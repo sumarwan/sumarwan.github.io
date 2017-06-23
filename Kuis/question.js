cef78e2449311f7385f2d3356734be061b7785b9
sumarwan <sumarwann12@gmail.com>
2017-06-23 07:35:58 +0700

    Add files via upload

---
+++ Kuis/question.js
@@ -0,0 +1,71 @@
+var questions = [{
+		"question": "Hasil perkalian dari 15 x 5 = ",
+		"option1": "60",
+		"option2": "35",
+		"option3": "20",
+		"option4": "75",
+		"answer": "4"
+}, {
+		"question": "Hasil penjumlahan dari 42 + 257 = ",
+		"option1": "300",
+		"option2": "299",
+		"option3": "197",
+		"option4": "297",
+		"answer": "2"
+}, {
+		"question": "Hasil pembagian dari 144 : 8 = ",
+		"option1": "6",
+		"option2": "19",
+		"option3": "18",
+		"option4": "32",
+		"answer": "3"
+}, {
+		"question": "Hasil dari 35 + 45 - 32 = ",
+		"option1": "31",
+		"option2": "55",
+		"option3": "48",
+		"option4": "64",
+		"answer": "3"
+}, {
+		"question": "Hasil dari 2 + 4 x 35 = ",
+		"option1": "142",
+		"option2": "143",
+		"option3": "144",
+		"option4": "145",
+		"answer": "1"
+}, {
+		"question": "Ibu membeli 950 bata merah. Digunakan untuk membuat dapur 675 bata merah. Berapa banyak bata merah yang tidak terpakai?",
+		"option1": "273",
+		"option2": "274",
+		"option3": "275",
+		"option4": "276",
+		"answer": "3"
+}, {
+		"question": "Hasil dari 2 - 100 : 5 = ",
+		"option1": "18",
+		"option2": "- 18",
+		"option3": "17",
+		"option4": "-17",
+		"answer": "2"
+}, {
+		"question": "Pak Sony memetik 972 buah Leci. leci tersebut dibeli oleh Pak Mahmud sebanyak 725 buah. Berapa buah leci yang masih ada?",
+		"option1": "227",
+		"option2": "447",
+		"option3": "347",
+		"option4": "247",
+		"answer": "4"
+}, {
+		"question": "20 + 18 = 18 + t, Jawaban t adalah...",
+		"option1": "22",
+		"option2": "32",
+		"option3": "20",
+		"option4": "18",
+		"answer": "3"
+}, {
+		"question": "Keranjang merah berisi 100 butir telur, keranjang hijau berisi 500 butir telur dan keranjang kuning berisi 505 butir telur. Yang lebih berat adalah keranjang ….",
+		"option1": "HIJAU",
+		"option2": "MERAH",
+		"option3": "KUNING",
+		"option4": "SAMA BERATNYA",
+		"answer": "3"
+}]
